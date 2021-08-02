ee34eb2a245432487f534ba44039e27eb514aea4
Tucker Gillbrand <tucker@gillbrand.com>
2021-08-02 09:09:50 -0700

    Initial Commit

---
+++ DietTracker/DietTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietTrackerApp.swift
+//  DietTracker
+//
+//  Created by Tucker(School) on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DietTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
